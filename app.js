6ce26911c32fd2f17dbad31f826db775b0567efe
Yuval Allweil <themarquisdesheric@gmail.com>
2017-02-27 13:34:47 -0800

    fixed images and song folder

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var button = document.querySelector('button');
 var audio = document.querySelector('audio');
 var section = document.querySelector('section');
 var counter = 0;
+var album1 = document.getElementById('album1');
+var album2 = document.getElementById('album2');
+var album3 = document.getElementById('album3');
+var album4 = document.getElementById('album4');
+var items = [];
+var indexOne = 0;
+var indexTwo = 1;
+var indexThree = 2;
+var indexFour = 3;
 
 var media = [
   {
@@ -39,24 +48,6 @@ var media = [
   // }
 ];
 
-var album1 = document.getElementById('Album1');
-var album2 = document.getElementById('Album2');
-var album3 = document.getElementById('Album3');
-var album4 = document.getElementById('Album4');
-var items = [];
-var indexOne = 0;
-var indexTwo = 1;
-var indexThree = 2;
-var indexFour = 3;
-
-button.addEventListener('click', playClickHandler);
-audio.src = media[0].song;
-section.addEventListener('click', imagesClickHandler);
-
-
-
-
-
 
 function playClickHandler() {
   audio.play();
@@ -76,21 +67,33 @@ function generateRandomNumber(){
 }
 
 function displayQuiz() {
-  var randomIndex1 = generateRandomNumber();
-  var randomIndex2 = generateRandomNumber();
-  var randomIndex3 = generateRandomNumber();
-  var randomIndex4 = generateRandomNumber();
-
- while (randomIndex1 === indexOne || randomIndex1 === randomIndex2 || randomIndex1 === randomIndex3) {
-    randomIndex1 = generateRandomNumber();
-  }
-  while (randomIndex2 === indexTwo || randomIndex2 === randomIndex1 || randomIndex2 === randomIndex3) {
-    randomIndex2 = generateRandomNumber();
-  }
-  while (randomIndex3 === indexThree || randomIndex3 === randomIndex2 || randomIndex3 === randomIndex1) {
-    randomIndex3 = generateRandomNumber();
-  }
-  while (randomIndex4 === indexFour || randomIndex4) {
-
-  }
+  album1.src = media[0].choice1;
+  album2.src = media[0].choice2;
+  album3.src = media[0].choice3;
+  album4.src = media[0].choice4;
 }
+
+button.addEventListener('click', playClickHandler);
+audio.src = media[0].song;
+section.addEventListener('click', imagesClickHandler);
+displayQuiz();
+
+
+// random logic
+// var randomIndex1 = generateRandomNumber();
+//   var randomIndex2 = generateRandomNumber();
+//   var randomIndex3 = generateRandomNumber();
+//   var randomIndex4 = generateRandomNumber();
+
+//  while (randomIndex1 === indexOne || randomIndex1 === randomIndex2 || randomIndex1 === randomIndex3) {
+//     randomIndex1 = generateRandomNumber();
+//   }
+//   while (randomIndex2 === indexTwo || randomIndex2 === randomIndex1 || randomIndex2 === randomIndex3) {
+//     randomIndex2 = generateRandomNumber();
+//   }
+//   while (randomIndex3 === indexThree || randomIndex3 === randomIndex2 || randomIndex3 === randomIndex1) {
+//     randomIndex3 = generateRandomNumber();
+//   }
+//   while (randomIndex4 === indexFour || randomIndex4) {
+
+//   }
