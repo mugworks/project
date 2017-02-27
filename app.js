3a4365e58a4859af516affe39b6a9e3170bb264e
Martin <martinconradpelayo@gmail.com>
2017-02-27 10:25:32 -0800

    branch2, start of game functionality

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 'use strict';
+var button = document.querySelector('button');
+var audio = document.querySelector('audio');
+button.addEventListener('click', clickHandler);
+
+function playlClickHandler() {
+  audio.play();
+  counter += 1;
+}
+
+
+
+
+
+
+
+
+
+
+var album1 = document.getElementById('Album1');
+var album2 = document.getElementById('Album2');
+var album3 = document.getElementById('Album3');
+var album4 = document.getElementById('Album4');
+
+
+var counter = 0;
+if(counter === 1){
+    displayChart();
+}
+
+funtion displayChart() {
+
+
+}
+
+
+
+
+functon clikcHandler(){
+
+var testSong = document.getElementById('button')
+}
