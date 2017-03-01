d6ae6297b446f8ec3f67ef72ec5426a71c71b583
Martin <martinconradpelayo@gmail.com>
2017-02-28 17:01:52 -0800

    little work on Local Storage

--- storage.js
+++ storage.js
@@ -1,42 +1,24 @@
 'use strict';
-        //Sets storage for username and emil\\
-//May want to make a conditional statement that tests if the
-//username and email has been submited and if so, place data in storage
-function nameEmailStorage(){
+          //Sets storage for username and email
+var form = document.querySelector('form');
+
+function submitHandler(){
   var nameDomElement = document.getElementById('username');
   var emailDomElement = document.getElementById('email');
 
-  var nameData = nameDomElement;
+  var nameData = nameDomElement.value;
   JSON.stringify(nameData);
-  localStorage.setItem('nameString', nameDate);
+  localStorage.setItem('nameString', nameData);
 
-  var emailData = emailDomElement;
+  var emailData = emailDomElement.value;
   JSON.stringify(emailData);
   localStorage.setItem('emailString', emailData);
 }
-  
+
+            //Set storage data for score
+var getScoreString = localStorage.getItem('score');
+var scoreData = JSON.parse(getScoreString);
 
 
 
-
-        //Set storage data for score\\
-function scoreStorage(){
-  var scoreData = userScore;
-  JSON.stringify(scoreData);
-  localStorage.setItem('scoreString', scoreData);
-}
-
-
-var getScoreString = localStorage.getItem('scoreString');
-var userScore = JSON.parse(getScoreString);;      //retrieve storage data and place on page
-//Could make another contitional statement that tests is user click
-//the end game button, the program retrieves storage and places data
-//in form or soemthing
-// 
-
-
-
-// var scoreString = localStorage.getItem('theUserScore');
-// var userScore = JSON.parse(scoreString);
-
-
+form.addEventListener('submit', submitHandler);
