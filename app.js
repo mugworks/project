95a78315ced53c57ce4bef5dd3677852cd6e9f50
Yuval Allweil <themarquisdesheric@gmail.com>
2017-03-01 10:28:33 -0800

    Added album info, ready to be displayed

--- app.js
+++ app.js
@@ -10,36 +10,37 @@ var userScore = 0;
 var media = [
   {             //song src
     song: 'songs/Cranes in the Sky.mp3',
-    choice1: 'images/solange.jpg',
-    choice2: 'images/lemonade.jpg',
-    choice3: 'images/rihanna.jpg',
-    choice4: 'images/laurynhill.jpg',
-                //answer                  album            artist
+                //image                  album            artist
+    choice1: ['images/solange.jpg', 'A Seat at the Table', 'Solange'],
+    choice2: ['images/lemonade.jpg', 'Lemonade', 'Beyonce'],
+    choice3: ['images/rihanna.jpg', 'Unapologetic', 'Rihanna'],
+    choice4: ['images/laurynhill.jpg', 'The Miseducation of Lauren Hill', 'Lauryn Hill'],
+                //answer
     answer: ['images/solange.jpg', 'A Seat at the Table', 'Solange']
   },
   {
     song: 'songs/01 Icky Thump (mp3cut.net).mp3',
-    choice1: 'images/ickythump.jpeg',
-    choice2: 'http://placehold.it/320x320',
-    choice3: 'http://placehold.it/320x320',
-    choice4: 'http://placehold.it/320x320',
+    choice1: ['images/ickythump.jpeg', 'Icky Thump', 'The White Stripes'],
+    choice2: ['images/deathfromabove.jpg', 'You\'re a Woman, I\'m a Machine', 'Death From Above 1979'],
+    choice3: ['images/blackkeysmagicpotion.jpg', 'Magic Potion', 'The White Stripes'],
+    choice4: ['images/apologiestothequeenmary.jpg', 'Apologies To The Queen Mary', 'Wolf Parade'],
     answer: ['images/ickythump.jpeg', 'Icky Thump', 'The White Stripes']
   },
   {
     song: 'songs/jackWhite.mp3',
-    choice1: 'images/lemonade.jpg',
-    choice2: 'images/rihanna.jpg',
-    choice3: 'images/laurynhill.jpg',
-    choice4: 'images/solange.jpg',
+    choice1: ['images/lemonade.jpg', 'Lemonade', 'Beyonce'],
+    choice2: ['images/rihanna.jpg', 'Unapologetic', 'Rihanna'],
+    choice3: ['images/laurynhill.jpg', 'The Miseducation of Lauren Hill', 'Lauryn Hill'],
+    choice4: ['images/solange.jpg', 'A Seat at the Table', 'Solange'],
     answer: ['images/lemonade.jpg', 'Lemonade', 'Beyonce']
   },
   {
-    song: 'songs/Cranes in the Sky.mp3',
-    choice1: 'images/solange.jpg',
-    choice2: 'http://placehold.it/320x320/fff/000',
-    choice3: 'http://placehold.it/320x320/fff/000',
-    choice4: 'http://placehold.it/320x320/fff/000',
-    answer: ['images/solange.jpg', 'A Seat at the Table', 'Solange']
+    song: 'songs/kurtvile.mp3',
+    choice1: ['images/believeimgoingdown.jpg', 'B\'lieve I\'m Goin Down...', 'Kurt Vile'],
+    choice2: ['images/mangylove.jpg', 'Mangy Love', 'Cass McCombs'],
+    choice3: ['images/singingsaw.jpg', 'Singing Saw', 'Kevin Morby'],
+    choice4: ['images/iloveyouhoneybear.jpg', 'I Love You Honeybear', 'Father John Misty'],
+    answer: ['images/believeimgoingdown.jpg', 'B\'lieve I\'m Goin Down...', 'Kurt Vile']
   }
 ];
 
@@ -92,28 +93,28 @@ function displayQuiz() {
 
   var rand1 = generateRandomNumber(albums.length);
   var rand2 = generateRandomNumber(albums.length);
-  while(rand2 === rand1){
+  while(rand2 === rand1) {
     rand2 = generateRandomNumber(albums.length);
   }
   var rand3 = generateRandomNumber(albums.length);
-  while(rand3 === rand2 || rand3 === rand1){
+  while(rand3 === rand2 || rand3 === rand1) {
     rand3 = generateRandomNumber(albums.length);
   }
   var rand4 = generateRandomNumber(albums.length);
-  while(rand4 === rand3 || rand4 === rand2 || rand4 === rand1){
+  while(rand4 === rand3 || rand4 === rand2 || rand4 === rand1) {
     rand4 = generateRandomNumber(albums.length);
   }
 
-  albums[rand1].src = media[userScore].choice1;
+  albums[rand1].src = media[userScore].choice1[0];
   albums[rand1].setAttribute('alt', media[userScore].answer[0]);
-  albums[rand2].src = media[userScore].choice2;
-  albums[rand3].src = media[userScore].choice3;
-  albums[rand4].src = media[userScore].choice4;
+  albums[rand2].src = media[userScore].choice2[0];
+  albums[rand3].src = media[userScore].choice3[0];
+  albums[rand4].src = media[userScore].choice4[0];
 }
 
 function grabQuizImages() {
   //Removes header image from array :D
-  for (var i = 4; i < images.length; i++) {
+  for (var i = 4; i < images.length - 1; i++) {
     albums.push(images[i]);
   }
 }
