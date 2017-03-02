334c9ef196e3112146043a43b1a9ef5345138fec
mugworks <greenwood.pdx@gmail.com>
2017-03-02 08:49:24 -0800

    fixed the look of the text and table on results page

--- results.js
+++ results.js
@@ -1,21 +1,19 @@
 'use strict';
 
-var comments = ['You got everything wrong! Grandma level!', 'Only one right. You need to unplug your ears.', 'Half right. Still need to increase your knowledge', 'Almost perfect. Maybe add a few albums to your collection', 'You are audiofile skills are strong', 'Wow! Perfect Score! You are a music junkie!'];
+var comments = ['Only one right. Grandma level.', 'Two right. Poser level.', 'Mid-level knowledge.', 'Your audiophile skills are strong', 'Wow! Perfect Score! You are a music junkie!'];
 
-var score = 5;
+var score = 2;
 
-if(score === 0){
-    // tableCell.setAttribute('id', '');
+if(score === 0) {
   var zeroComment = document.getElementById('zero_score_text');
   zeroComment.textContent = ('You got zero correct answers. Come to r&d immediately!');
-}
-else{
+} else {
   for (var i = 1; i <= score; i++) {
     var tableCell = document.getElementById('score' + [i]);
     tableCell.setAttribute('id', 'filledIn');
   }
   var tableComment = document.getElementById('status' + [score]);
-  tableComment.textContent = comments[score];
+  tableComment.textContent = comments[score - 1];
 
   if (score === 5) {
     var paraText = document.getElementById('congrats');
@@ -25,5 +23,4 @@ else{
     paraOther.textContent = ('Thanks for playing!');
   }
 };
-// comment for github
 
