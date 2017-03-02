7ae50e779bbd9e1662eeabcbdb9cb8d9c6239092
Jane Smith <example@email.com>
2017-03-02 10:31:30 -0800

    added local storage to resulst.js

--- results.js
+++ results.js
@@ -2,20 +2,20 @@
 
 var comments = ['Only one right. Grandma level.', 'Two right. Poser level.', 'Mid-level knowledge.', 'Your audiophile skills are strong', 'Wow! Perfect Score! You are a music junkie!'];
 
-var score = 2;
-
-if(score === 0) {
+// var score = 2;
+loadHandler();
+if(loadScore === 0) {
   var zeroComment = document.getElementById('zero_score_text');
   zeroComment.textContent = ('You got zero correct answers. Come to r&d immediately!');
 } else {
-  for (var i = 1; i <= score; i++) {
+  for (var i = 1; i <= loadScore; i++) {
     var tableCell = document.getElementById('score' + [i]);
     tableCell.setAttribute('id', 'filledIn');
   }
-  var tableComment = document.getElementById('status' + [score]);
-  tableComment.textContent = comments[score - 1];
+  var tableComment = document.getElementById('status' + [loadScore]);
+  tableComment.textContent = comments[loadScore - 1];
 
-  if (score === 5) {
+  if (loadScore === 5) {
     var paraText = document.getElementById('congrats');
     paraText.textContent = ('Congratulations! You get a free record voucher!');
   } else {
